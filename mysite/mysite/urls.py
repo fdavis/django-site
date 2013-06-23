7ca9b7d64ddaf3a1c98497c09dc0f5949d5c6bd9
fdavis <reddot357@hotmail.com>
2013-06-22 19:13:12 -0700

    add admin url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import hello, current_datetime, time_plus
+from django.contrib import admin
+admin.autodiscover
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', time_plus),
+    url(r'^admin/', include(admin.site.urls)),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
