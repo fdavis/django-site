4f6ff285f209240d8a92bfb4743dd018a2b793d6
fdavis <reddot357@hotmail.com>
2013-06-22 23:47:38 -0700

    try old meta printing, remove extra requests

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -5,11 +5,12 @@ def hello(request):
   return render(request, 'hello.html') 
 
 def show_meta(request):
-  values = request.META
-  values["request.path"] = request.path
-  values["request.get_host"] = request.get_host()
-  values['request.get_full_path'] = request.get_full_path()
-  values['request.is_secure'] = request.is_secure()
+  values = request.META.items()
+  values.sort()
+  #values["request.path"] = request.path
+  #values["request.get_host"] = request.get_host()
+  #values['request.get_full_path'] = request.get_full_path()
+  #values['request.is_secure'] = request.is_secure()
   #html = []
   #for k, v in values:
   #  html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
