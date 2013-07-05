e1afa8e0eba44959085300b07ac7d394a8a72467
fdavis <reddot357@hotmail.com>
2013-07-04 17:35:38 -0700

    default to hello page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,7 +9,7 @@ admin.autodiscover
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^hello/$', hello),
+    url(r'^hello/$|^$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', time_plus),
     url(r'^admin/$', include(admin.site.urls)),
